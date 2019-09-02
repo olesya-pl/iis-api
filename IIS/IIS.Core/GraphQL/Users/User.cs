d3c1a4962a32c73d162359e995ac78e83113265c
Heorhii Kuzmenko <hkuzmenko@warfare-tec.com>
2019-09-02 18:09:17 +0300

    fix(graphql): Quickfix user id type
    Relates IIS-4141

--- IIS/IIS.Core/GraphQL/Users/User.cs
+++ IIS/IIS.Core/GraphQL/Users/User.cs
@@ -1,12 +1,13 @@
 using System;
 using HotChocolate;
+using HotChocolate.Types;
 using IIS.Core.GraphQL.Common;
 
 namespace IIS.Core.GraphQL.Users
 {
     public class User
     {
-        public Guid Id { get; set; }
+        [GraphQLType(typeof(NonNullType<IdType>))] public Guid Id { get; set; }
         [GraphQLNonNullType] public string Name { get; set; }
         [GraphQLNonNullType]public string Username { get; set; }
         public bool IsBlocked { get; set; }
