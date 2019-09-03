a7a7f58308d87a97732fd4d0ecaecd19c0a6144d
Heorhii Kuzmenko <hkuzmenko@warfare-tec.com>
2019-09-03 18:08:05 +0300

    fix(graphql): FileInfo id type fix
    Relates IIS-4111

--- IIS/IIS.Core/GraphQL/Files/FileInfo.cs
+++ IIS/IIS.Core/GraphQL/Files/FileInfo.cs
@@ -1,10 +1,12 @@
 using System;
+using HotChocolate;
+using HotChocolate.Types;
 
 namespace IIS.Core.GraphQL.Files
 {
     public class FileInfo
     {
-        public Guid Id { get; set; }
+        [GraphQLType(typeof(NonNullType<IdType>))] public Guid Id { get; set; }
         public string Name { get; set; }
         public string ContentType { get; set; }
         public bool IsTemporary { get; set; }
