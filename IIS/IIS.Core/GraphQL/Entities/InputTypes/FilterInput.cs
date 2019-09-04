6e27718436051a5d052789ea433c42d4a820c25e
Heorhii Kuzmenko <hkuzmenko@warfare-tec.com>
2019-09-04 16:50:02 +0300

    fix(graphql) EntityTypes quickfix
    Relates IIS-4066

--- IIS/IIS.Core/GraphQL/Entities/InputTypes/FilterInput.cs
+++ IIS/IIS.Core/GraphQL/Entities/InputTypes/FilterInput.cs
@@ -5,9 +5,9 @@ namespace IIS.Core.GraphQL.Entities.InputTypes
 {
     public class FilterInput
     {
-        [GraphQLNonNullType] public string SearchQuery { get; set; }
+        public string SearchQuery { get; set; }
 
-        [GraphQLNonNullType] public string Suggestion { get; set; }
+        public string Suggestion { get; set; }
 
         [GraphQLType(typeof(NotImplementedType))]
         public string Search { get; set; } // ConditionPredicate
