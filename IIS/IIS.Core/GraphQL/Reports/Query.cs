b1c3e56b551acd297f6fdb153de6fcb5a3046f4f
imuratov <imuratov@warfare-tec.com>
2019-10-10 20:46:09 +0300

    feat(reports): CRUD IIS-4242 get report by id

--- IIS/IIS.Core/GraphQL/Reports/Query.cs
+++ IIS/IIS.Core/GraphQL/Reports/Query.cs
@@ -1,4 +1,5 @@
 using HotChocolate;
+using HotChocolate.Types;
 using IIS.Core.GraphQL.Common;
 using IIS.Core.Ontology.EntityFramework.Context;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,12 @@ namespace IIS.Core.GraphQL.Reports
             var result = await query.Select(row => new Report(row.Id, row.Title, row.Recipient, row.CreatedAt)).ToListAsync();
             var totalCount = await context.Reports.CountAsync();
             return new GraphQLCollection<Report>(result, totalCount);
+        }
+
+        public async Task<Report> GetReport([Service] OntologyContext context, [GraphQLType(typeof(NonNullType<IdType>))] Guid reportId)
+        {
+            var dbReport = await context.Reports.FindAsync(reportId);
+            return new Report(dbReport.Id, dbReport.Title, dbReport.Recipient, dbReport.CreatedAt);
         }
     }
 }
