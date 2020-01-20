0cf3fd926380bb9015387ae641b8b1a1d9254a94
Oleksandr Rapota <orapota@warfare-tec.com>
2020-01-20 16:05:32 +0200

    refactor(ontology): reorganized contour ontology
    IIS-4439 Онтология - реализация новой схемы по человеку

--- IIS/IIS.Core/Ontology/Meta/EntityMeta.cs
+++ IIS/IIS.Core/Ontology/Meta/EntityMeta.cs
@@ -4,6 +4,7 @@ namespace IIS.Core.Ontology.Meta
 {
     public class EntityMeta : IMeta
     {
+        public int? SortOrder { get; set; }
         public bool? ExposeOnApi { get; set; }
         public bool? HasFewEntities { get; set; }
         public EntityOperation[] AcceptsEmbeddedOperations { get; set; }
