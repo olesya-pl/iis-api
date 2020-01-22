b7fae4949deeb18106666dce228488087c39595d
Yuriy Dolzhykov <yuriydolzhikov@gmail.com>
2020-01-22 16:32:42 +0200

    feat(elastic): add classes and methods for readong ExtNode from DB
    Relates to: IIS-4479

---
+++ src/Iis.Api/Ontology.EntityFramework/ExtNodeService.cs
@@ -0,0 +1,62 @@
+﻿using Iis.DataModel;
+using Iis.Domain.ExtendedData;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Iis.Api.Ontology.EntityFramework
+{
+    public class ExtNodeService
+    {
+        private readonly OntologyContext _context;
+
+        public ExtNodeService(OntologyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ExtNode> MapExtNode(NodeEntity nodeEntity)
+        {
+            var extNode = new ExtNode
+            {
+                Id = nodeEntity.Id,
+                NodeTypeId = nodeEntity.NodeTypeId,
+                NodeTypeName = nodeEntity.NodeType?.Name,
+                AttributeValue = nodeEntity.Attribute?.Value,
+                CreatedAt = nodeEntity.CreatedAt,
+                UpdatedAt = nodeEntity.UpdatedAt,
+                Children = await GetExtNodesByIds(nodeEntity.OutgoingRelations.Select(r => r.Id))
+            };
+            return await Task.FromResult(extNode);
+        }
+        
+        public async Task<ExtNode> GetExtNodeById(Guid id)
+        {
+            var nodeEntity = await GetNodeQuery()
+                .Where(n => n.Id == id)
+                .SingleOrDefaultAsync();
+            return await MapExtNode(nodeEntity);
+        }
+
+        public async Task<List<ExtNode>> GetExtNodesByIds(IEnumerable<Guid> ids)
+        {
+            var nodes = await GetNodeQuery()
+                .Where(n => ids.Contains(n.Id))
+                .ToListAsync();
+
+            var result = nodes.Select(async n => await MapExtNode(n)).Select(t => t.Result).ToList();
+            return result;
+        }
+
+        private IQueryable<NodeEntity> GetNodeQuery()
+        {
+            return _context.Nodes
+                .Include(n => n.Attribute)
+                .Include(n => n.NodeType)
+                .Include(n => n.OutgoingRelations)
+                .ThenInclude(r => r.TargetNode);
+        }
+    }
+}
---
+++ src/Iis.Domain/ExtendedData/ExtNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.Domain.ExtendedData
+{
+    public class ExtNode
+    {
+        public Guid Id { get; set; }
+        public Guid NodeTypeId { get; set; }
+        public string NodeTypeName { get; set; }
+        public string AttributeValue { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public List<ExtNode> Children { get; set; } = new List<ExtNode>();
+    }
+}
