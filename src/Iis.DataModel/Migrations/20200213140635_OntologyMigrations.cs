7e1a3d22d3e6bdc54bdfa1ddddcc265e8c9a3ee3
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2020-02-13 16:07:52 +0200

    feat(ontology): table OntologyMigrations
    Relates to IIS-4664

---
+++ src/Iis.DataModel/Migrations/20200213140635_OntologyMigrations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IIS.Core.Migrations
+{
+    public partial class OntologyMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OntologyMigrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    OrderNumber = table.Column<int>(nullable: false),
+                    StructureBefore = table.Column<string>(nullable: true),
+                    StructureAfter = table.Column<string>(nullable: true),
+                    MigrationRules = table.Column<string>(nullable: true),
+                    Log = table.Column<string>(nullable: true),
+                    StartTime = table.Column<DateTime>(nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    IsSuccess = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OntologyMigrations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OntologyMigrations");
+        }
+    }
+}
---
+++ src/Iis.DataModel/OntologyMigrationsEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.DataModel
+{
+    public class OntologyMigrationsEntity : BaseEntity
+    {
+        public int OrderNumber { get; set; }
+        public string StructureBefore { get; set; }
+        public string StructureAfter { get; set; }
+        public string MigrationRules { get; set; }
+        public string Log { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
