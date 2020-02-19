f21f1a182bb3a971077db403b5e518e0b6c2e59c
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2020-02-19 18:05:34 +0200

    feat(core): config file in browser
    Relates to IIS-4699

---
+++ src/Iis.Api/Controllers/ConfigController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Iis.Api.Controllers
+{
+    [Route("{controller}")]
+    [ApiController]
+    public class ConfigController: Controller
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> Get(CancellationToken token)
+        {
+            var configName = "appsettings.json";
+            if (!System.IO.File.Exists(configName))
+            {
+                return Content($"There is noconfig file with name {configName}");
+            }
+            var configText = System.IO.File.ReadAllText(configName);
+            return Content(configText);
+        }
+    }
+}
