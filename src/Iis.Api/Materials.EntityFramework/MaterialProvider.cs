a6017e571e09cac89906ca8237e46da4693f0231
Bohdan Stupak <bstupak@warfare-tec.com>
2020-04-13 12:55:29 +0300

    fix(material): Missing include for a single material
    Relates to IIS-5060

--- src/Iis.Api/Materials.EntityFramework/MaterialProvider.cs
+++ src/Iis.Api/Materials.EntityFramework/MaterialProvider.cs
@@ -180,6 +180,7 @@ namespace IIS.Core.Materials.EntityFramework
                     .Include(m => m.Relevance)
                     .Include(m => m.Completeness)
                     .Include(m => m.SourceReliability)
+                    .Include(m => m.Children)
                     .Include(m => m.MaterialInfos)
                     .ThenInclude(m => m.MaterialFeatures)
                     .AsNoTracking();
