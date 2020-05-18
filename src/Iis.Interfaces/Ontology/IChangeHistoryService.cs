41d26c0afb02ef498a3493172d3e40c2a8b1c4f1
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2020-05-18 22:28:23 +0300

    feat(ontology): Change History
    Relates to IIS-5158

--- src/Iis.Interfaces/Ontology/IChangeHistoryService.cs
+++ src/Iis.Interfaces/Ontology/IChangeHistoryService.cs
@@ -6,7 +6,7 @@ namespace Iis.Interfaces.Ontology
 {
     public interface IChangeHistoryService
     {
-        Task SaveChange(Guid typeId, Guid rootTypeId, Guid targetId, string userName, string oldValue, string newValue);
+        Task SaveChange(string attributeDotName, Guid targetId, string userName, string oldValue, string newValue);
         Task<IReadOnlyList<IChangeHistoryItem>> GetChangeHistory(Guid targetId, string propertyName);
     }
 }
