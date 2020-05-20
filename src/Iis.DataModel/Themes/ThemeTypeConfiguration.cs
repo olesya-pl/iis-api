4f77f5bc34cb5ab0f1070ebd94f413bccdf82dc8
mdemydenko <mdemydenko@warfare-tec.com>
2020-05-20 17:53:28 +0300

    fix ThemeType seed

--- src/Iis.DataModel/Themes/ThemeTypeConfiguration.cs
+++ src/Iis.DataModel/Themes/ThemeTypeConfiguration.cs
@@ -33,7 +33,7 @@ namespace Iis.DataModel.Themes
             builder.HasData(
                 new ThemeTypeEntity{Id = new Guid("2b8fd109-cf4a-4f76-8136-de761da53d20"), ShortTitle = "М", Title = "Матеріал", EntityTypeName = "EntityMaterial"},
                 new ThemeTypeEntity{Id = new Guid("043ae699-e070-4336-8513-e90c87555c58"), ShortTitle = "О", Title = "Об'єкт", EntityTypeName = "EntityObject"},
-                new ThemeTypeEntity{Id = new Guid("2b8fd109-cf4a-4f76-8136-de761da53d20"), ShortTitle = "П", Title = "Подія", EntityTypeName = "EntityEvent"}
+                new ThemeTypeEntity{Id = new Guid("42f61965-8baa-4026-ab33-0378be8a6c3e"), ShortTitle = "П", Title = "Подія", EntityTypeName = "EntityEvent"}
             );
         }
     }
