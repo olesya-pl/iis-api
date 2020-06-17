8d3c5523106b7893c9af0faaa6242d5e6ad47d3b
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2020-06-17 11:01:36 +0300

    fix(ontology): aliases
    Relates to IIS-5415

---
+++ src/Iis.DataModel/AliasConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Iis.DataModel
+{
+    internal sealed class AliasConfiguration : IEntityTypeConfiguration<AliasEntity>
+    {
+        public void Configure(EntityTypeBuilder<AliasEntity> builder)
+        {
+            builder
+                .Property(p => p.Id)
+                .ValueGeneratedNever();
+        }
+    }
+}
---
+++ src/Iis.DataModel/AliasEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.DataModel
+{
+    public class AliasEntity: BaseEntity
+    {
+        public string DotName { get; set; }
+        public string Value { get; set; }
+    }
+}
