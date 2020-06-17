fca96420801d55f5377be209899f992e4c6c93c1
Bohdan Stupak <bstupak@warfare-tec.com>
2020-06-17 15:52:25 +0300

    feat (change history): group item by history id
    relates to: IIS-5509

---
+++ src/Iis.Api/GraphQL/ChangeHistory/ChangeHistoryItemGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using HotChocolate;
+using HotChocolate.Types;
+
+namespace IIS.Core.GraphQL.ChangeHistory
+{
+    public class ChangeHistoryItemGroup
+    {
+        [GraphQLType(typeof(IdType))]
+        public Guid RequestId { get; set; }
+
+        public List<ChangeHistoryItem> Items { get; set; }
+    }
+}
