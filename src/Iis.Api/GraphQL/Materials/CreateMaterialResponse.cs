f7c088ddd7059dfbf485bb15e8a642923f7c7d28
Bohdan Stupak <bstupak@warfare-tec.com>
2020-07-01 15:07:55 +0300

    refactor: remove extra db call
    relates to: IIS-5616

---
+++ src/Iis.Api/GraphQL/Materials/CreateMaterialResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HotChocolate;
+using HotChocolate.Types;
+
+namespace IIS.Core.GraphQL.Materials
+{
+    public class CreateMaterialResponse
+    {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
+    }
+}
