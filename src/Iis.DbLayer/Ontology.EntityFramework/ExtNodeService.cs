7d4e630480bb9794abff9c030ff9b812a042bb26
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2020-07-07 11:59:15 +0300

    fix(ontology): revert wrong renaming
    Relates to IIS-5620

--- src/Iis.DbLayer/Ontology.EntityFramework/ExtNodeService.cs
+++ src/Iis.DbLayer/Ontology.EntityFramework/ExtNodeService.cs
@@ -61,7 +61,7 @@ namespace Iis.DbLayer.Ontology.EntityFramework
         {
             if (nodeEntity.Attribute == null) return null;
 
-            var scalarType = nodeEntity.INodeTypeModel.IAttributeTypeModel.ScalarType;
+            var scalarType = nodeEntity.NodeType.IAttributeTypeModel.ScalarType;
             var value = nodeEntity.Attribute.Value;
             switch (scalarType)
             {
@@ -82,9 +82,9 @@ namespace Iis.DbLayer.Ontology.EntityFramework
                 NodeTypeId = nodeEntity.NodeTypeId.ToString("N"),
                 NodeTypeName = nodeTypeName,
                 NodeTypeTitle = nodeTypeTitle,
-                EntityTypeName = nodeEntity.INodeTypeModel.Name,
+                EntityTypeName = nodeEntity.NodeType.Name,
                 AttributeValue = GetAttributeValue(nodeEntity),
-                ScalarType = nodeEntity.INodeTypeModel?.IAttributeTypeModel?.ScalarType,
+                ScalarType = nodeEntity.NodeType?.IAttributeTypeModel?.ScalarType,
                 CreatedAt = nodeEntity.CreatedAt,
                 UpdatedAt = nodeEntity.UpdatedAt,
                 Children = await GetExtNodesByRelations(nodeEntity.OutgoingRelations, cancellationToken)
@@ -100,7 +100,7 @@ namespace Iis.DbLayer.Ontology.EntityFramework
             
             if (nodeEntity == null) return null;
 
-            var extNode = await MapExtNodeAsync(nodeEntity, nodeEntity.INodeTypeModel.Name, nodeEntity.INodeTypeModel.Title, cancellationToken);
+            var extNode = await MapExtNodeAsync(nodeEntity, nodeEntity.NodeType.Name, nodeEntity.NodeType.Title, cancellationToken);
             return extNode;
         }
 
@@ -112,7 +112,7 @@ namespace Iis.DbLayer.Ontology.EntityFramework
                 var node = await GetNodeQuery().Where(node => node.Id == relation.TargetNodeId).SingleOrDefaultAsync();
                 if (!ObjectOfStudyTypes.Contains(node.NodeTypeId))
                 {
-                    var extNode = await MapExtNodeAsync(node, relation.Node.INodeTypeModel.Name, relation.Node.INodeTypeModel.Title ,cancellationToken);
+                    var extNode = await MapExtNodeAsync(node, relation.Node.NodeType.Name, relation.Node.NodeType.Title ,cancellationToken);
                     result.Add(extNode);
                 }
             }
@@ -132,7 +132,7 @@ namespace Iis.DbLayer.Ontology.EntityFramework
 
             foreach (var node in nodes)
             {
-                Console.WriteLine($"{++cnt}/{total}: {node.Id};{node.INodeTypeModel.Name}");
+                Console.WriteLine($"{++cnt}/{total}: {node.Id};{node.NodeType.Name}");
                 var extNode = await GetExtNodeByIdAsync(node.Id, cancellationToken);
                 result.Add(extNode);
             }
@@ -144,11 +144,11 @@ namespace Iis.DbLayer.Ontology.EntityFramework
         {
             return _context.Nodes
                 .Include(n => n.Attribute)
-                .Include(n => n.INodeTypeModel)
+                .Include(n => n.NodeType)
                 .ThenInclude(nt => nt.IAttributeTypeModel)
                 .Include(n => n.OutgoingRelations)
                 .ThenInclude(r => r.Node)
-                .ThenInclude(rn => rn.INodeTypeModel)
+                .ThenInclude(rn => rn.NodeType)
                 .Include(n => n.OutgoingRelations)
                 .ThenInclude(r => r.TargetNode)
                 .Where(n => !n.IsArchived);
--- src/Iis.UnitTests/Materials/MaterialProviderTests.cs
+++ src/Iis.UnitTests/Materials/MaterialProviderTests.cs
@@ -97,7 +97,7 @@ namespace Iis.UnitTests.Materials
                     info.Data = "{}";
                     foreach (var feature in info.MaterialFeatures)
                     {
-                        feature.Node.INodeTypeModel.Meta = null;
+                        feature.Node.NodeType.Meta = null;
                     }
                 }
             }
