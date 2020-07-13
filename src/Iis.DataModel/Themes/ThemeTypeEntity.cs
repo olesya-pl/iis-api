4a0817ac7c62d780c4225f8794e3c2a0cd0b1f1e
mdemydenko <mdemydenko@warfare-tec.com>
2020-07-13 18:37:02 +0300

    added static identifiers for ThemeType

--- src/Iis.DataModel/Themes/ThemeTypeEntity.cs
+++ src/Iis.DataModel/Themes/ThemeTypeEntity.cs
@@ -1,9 +1,18 @@
+using System;
 namespace Iis.DataModel.Themes
 {
     public class ThemeTypeEntity : BaseEntity
     {
-        public string ShortTitle {get;set;}
-        public string Title {get;set;}
-        public string EntityTypeName {get;set;}
+        public string ShortTitle { get; set; }
+        public string Title { get; set; }
+        public string EntityTypeName { get; set; }
+        public static readonly Guid EntityMapId
+            = new Guid("2692d888-6274-482e-a198-02c24436df3f");
+        public static readonly Guid EntityMaterialId
+            = new Guid("2b8fd109-cf4a-4f76-8136-de761da53d20");
+        public static readonly Guid EntityObjectId
+            = new Guid("043ae699-e070-4336-8513-e90c87555c58");
+        public static readonly Guid EntityEventId
+            = new Guid("42f61965-8baa-4026-ab33-0378be8a6c3e");
     }
 }
