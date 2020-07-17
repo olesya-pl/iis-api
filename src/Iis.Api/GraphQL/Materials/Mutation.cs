8bc6fb6702fe767a68b53857b855422bb05c008c
Bohdan Stupak <bstupak@warfare-tec.com>
2020-07-17 15:07:01 +0300

    feat (materials): create materials in controller

--- src/Iis.Api/GraphQL/Materials/Mutation.cs
+++ src/Iis.Api/GraphQL/Materials/Mutation.cs
@@ -8,29 +8,6 @@ namespace IIS.Core.GraphQL.Materials
 {
     public class Mutation
     {
-        // ReSharper disable once UnusedMember.Global
-        public async Task<CreateMaterialResponse> CreateMaterial(
-            [Service] IMaterialProvider materialProvider,
-            [Service] IMaterialService materialService,
-            [Service] IMapper mapper,
-            [Service] IFeatureProcessorFactory featureProcessorFactory,
-            [GraphQLNonNullType] MaterialInput input)
-        {
-            Iis.Domain.Materials.Material inputMaterial = mapper.Map<Iis.Domain.Materials.Material>(input);
-
-            inputMaterial.Reliability = materialProvider.GetMaterialSign(input.ReliabilityText);
-
-            inputMaterial.SourceReliability = materialProvider.GetMaterialSign(input.SourceReliabilityText);
-
-            inputMaterial.LoadData = mapper.Map<Iis.Domain.Materials.MaterialLoadData>(input);
-
-            inputMaterial.Metadata = await featureProcessorFactory.GetInstance(inputMaterial.Source, inputMaterial.Type).ProcessMetadata(inputMaterial.Metadata);
-
-            await materialService.SaveAsync(inputMaterial);
-            
-            return new CreateMaterialResponse { Id = inputMaterial.Id };
-        }
-
         public async Task<Material> UpdateMaterial(
             [Service] IMaterialService materialService,
             [Service] IMapper mapper,
