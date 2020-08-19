2702305a1aec484a76d63cf230fd5dede7e6f1f0
Taras Mykhats <tmykhats@warfare-tec.com>
2020-08-19 13:23:18 +0300

    use novell ldap library instead of DirectoryServie.Protocols due to the last is not working on no-windows platform

--- src/Iis.UnitTests/Services/ActiveDirectoryTests.cs
+++ src/Iis.UnitTests/Services/ActiveDirectoryTests.cs
@@ -14,7 +14,6 @@ namespace Iis.UnitTests.Services
             _testOutputHelper = testOutputHelper;
         }
 
-
         [Fact(Skip = "only for debug")]
         public void GetAllGroups()
         {
@@ -32,7 +31,9 @@ namespace Iis.UnitTests.Services
         public void GetGroupsByIds()
         {
             var client = new ActiveDirectoryClient("192.168.88.31", "iis", "VPkqGPv!");
-            var groups = client.GetGroupsByIds(Guid.Parse("d6e91b97-a252-489a-9132-596d48b1af7f"), Guid.Parse("a5490dde-8248-4476-bb4c-ace52b4f8f50"));
+            var groups = client.GetGroupsByIds(
+                Guid.Parse("d6e91b97-a252-489a-9132-596d48b1af7f"),
+                Guid.Parse("a5490dde-8248-4476-bb4c-ace52b4f8f50"));
 
             _testOutputHelper.WriteLine($"Count: {groups.Count}");
             foreach (var sr in groups)
