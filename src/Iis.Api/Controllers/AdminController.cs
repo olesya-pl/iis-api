054fc0977199cc30f313af9694bc886bb8497ad9
mdemydenko <mdemydenko@warfare-tec.com>
2020-08-19 13:42:18 +0300

    removed Data array from ES and furher results

--- src/Iis.Api/Controllers/AdminController.cs
+++ src/Iis.Api/Controllers/AdminController.cs
@@ -100,8 +100,6 @@ namespace Iis.Api.Controllers
                 new ElasticMappingProperty("Metadata.RegTime", ElasticMappingPropertyType.Date, formats:ElasticConfiguration.DefaultDateFormats),
                 new ElasticMappingProperty("CreatedDate", ElasticMappingPropertyType.Date, formats:ElasticConfiguration.DefaultDateFormats),
                 new ElasticMappingProperty("LoadData.ReceivingDate", ElasticMappingPropertyType.Date, formats:ElasticConfiguration.DefaultDateFormats),
-                new ElasticMappingProperty("Data.Text", ElasticMappingPropertyType.Text),
-                new ElasticMappingProperty("Children.Data.Text", ElasticMappingPropertyType.Text),
                 new ElasticMappingProperty("ParentId", ElasticMappingPropertyType.Keyword, true),
                 new ElasticMappingProperty("ParentId", ElasticMappingPropertyType.Keyword, true),
                 new ElasticMappingProperty("ImageVector", ElasticMappingPropertyType.DenseVector, dimensions:MaterialDocument.ImageVectorDimensionsCount)
