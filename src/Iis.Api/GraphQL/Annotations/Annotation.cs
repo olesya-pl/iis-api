5b225de6f068e18e2eff8cdf9591b2fafcb08b3f
mdemydenko <mdemydenko@warfare-tec.com>
2020-08-20 17:30:55 +0300

    removed base Annotation

--- src/Iis.Api/GraphQL/Annotations/Annotation.cs
+++ src/Iis.Api/GraphQL/Annotations/Annotation.cs
@@ -1,12 +1,17 @@
+using System;
 using HotChocolate;
+using HotChocolate.Types;
 using Newtonsoft.Json.Linq;
 using IIS.Core.GraphQL.Scalars;
 
 namespace IIS.Core.GraphQL.Annotations
 {
 
-    public class Annotation : BaseAnnotation
+    public class Annotation
     {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
+
         [GraphQLType(typeof(JsonScalarType))]
         public JObject Content {get;set;}
     }
--- src/Iis.Api/GraphQL/Annotations/AnnotationInput.cs
+++ src/Iis.Api/GraphQL/Annotations/AnnotationInput.cs
@@ -1,9 +1,13 @@
+using System;
 using HotChocolate;
+using HotChocolate.Types;
 
 namespace IIS.Core.GraphQL.Annotations
 {
-    public class AnnotationInput : BaseAnnotation
+    public class AnnotationInput
     {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
         [GraphQLNonNullType]
         public string Content { get; set; }
     }
