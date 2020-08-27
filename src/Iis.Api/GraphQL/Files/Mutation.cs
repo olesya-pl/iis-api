359ae3be797a8d268f8e78b09b0777a7ce8fb150
Bohdan Stupak <bstupak@warfare-tec.com>
2020-08-27 14:30:25 +0300

    feat: upload png
    relates to: IIS-5978

--- src/Iis.Api/GraphQL/Files/Mutation.cs
+++ src/Iis.Api/GraphQL/Files/Mutation.cs
@@ -1,22 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using HotChocolate;
 using Iis.Api.Configuration;
+using Iis.Domain.Materials;
+using IIS.Core.Files;
+using IIS.Core.Materials;
+using Newtonsoft.Json.Linq;
 
 namespace IIS.Core.GraphQL.Files
 {
     public class Mutation
     {
         public async Task<UploadResult> Upload([Service] UploadConfiguration uploadConfiguration,
+            [Service] IFileService fileService,
+            [Service] IMaterialService materialService,
             UploadInput input)
         {
-            string destinationDir;
             if (input.Name.EndsWith(".docx"))
             {
-                destinationDir = uploadConfiguration.DocxDirectory;
+                return UploadDocx(uploadConfiguration, input);
+            }
+            else if (input.Name.EndsWith(".png"))
+            {
+                return await UploadPng(fileService, materialService, input);
             }
             else
             {
@@ -26,25 +34,56 @@ namespace IIS.Core.GraphQL.Files
                     Message = "File not supported"
                 };
             }
+        }
 
-            try
+        private static async Task<UploadResult> UploadPng(IFileService fileService, IMaterialService materialService, UploadInput input)
+        {
+            using (var stream = new MemoryStream(input.Content))
             {
-                var byteArray = input.Content;
-                var fileName = System.IO.Path.Combine(destinationDir, input.Name);
-                using var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                FileId fileSaveResult = await fileService
+                .SaveFileAsync(stream, input.Name, "image/png", CancellationToken.None);
+                if (fileSaveResult.IsDuplicate)
+                {
+                    return new UploadResult
+                    {
+                        Success = false,
+                        Message = "File is duplicate"
+                    };
+                }
+
+                var material = new Material
+                {
+                    Id = Guid.NewGuid(),
+                    Title = input.Name,
+                    Type = "image",
+                    Source = "iis.api",
+                    Metadata = JObject.FromObject(new
+                    {
+                        source = "iis.api",
+                        type = "image"
+                    }),
+                    LoadData = new MaterialLoadData(),
+                    File = new Iis.Domain.Materials.FileInfo(fileSaveResult.Id)
+                };
+
+                await materialService.SaveAsync(material);
+
+                return new UploadResult { Success = true };
+            }
+        }
+
+        private static UploadResult UploadDocx(UploadConfiguration uploadConfiguration, UploadInput input)
+        {
+            var byteArray = input.Content;
+            var fileName = System.IO.Path.Combine(uploadConfiguration.DocxDirectory, input.Name);
+            using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
                 fs.Write(byteArray, 0, byteArray.Length);
                 return new UploadResult
                 {
                     Success = true
                 };
-            }
-            catch (Exception ex)
-            {
-                return new UploadResult()
-                {
-                    Success = false,
-                    Message = ex.Message
-                };
+
             }
         }
     }
