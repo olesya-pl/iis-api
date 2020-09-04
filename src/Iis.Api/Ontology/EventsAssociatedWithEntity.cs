f381d3f49acc59cffd0e63c053bbaaae48bd6dd0
Bohdan Stupak <bstupak@warfare-tec.com>
2020-09-04 11:10:45 +0300

    feat (events): add more event properties
    relates to: IIS-5944

---
+++ src/Iis.Api/Ontology/EventsAssociatedWithEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HotChocolate;
+using HotChocolate.Types;
+
+namespace Iis.Api.Ontology
+{
+    public class EventAssociatedWithEntity
+    {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? StartsAt { get; set; }
+        public DateTime? EndsAt { get; set; }
+        public EventState State { get; set; }
+        public EventImportance Importance { get; set; }
+    }
+
+    public class EventState
+    {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+
+    public class EventImportance
+    {
+        [GraphQLType(typeof(NonNullType<IdType>))]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
--- src/Iis.Api/Ontology/NodeToJObjectMapper.cs
+++ src/Iis.Api/Ontology/NodeToJObjectMapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Iis.Domain;
 using Newtonsoft.Json.Linq;
 
@@ -6,6 +8,16 @@ namespace Iis.Api.Ontology
 {
     public class NodeToJObjectMapper
     {
+        private readonly IOntologyService _ontologyService;
+
+        private readonly string[] EventDotNames = new[] {
+            "name", "description", "updatedAt", "startsAt", "endsAt" };
+
+        public NodeToJObjectMapper(IOntologyService ontologyService)
+        {
+            _ontologyService = ontologyService;
+        }
+
         public JObject NodeToJObject(Node node)
         {
             var result = new JObject(new JProperty(nameof(node.Id).ToLower(), node.Id.ToString("N")));
@@ -18,11 +30,47 @@ namespace Iis.Api.Ontology
             return result;
         }
 
+        public async Task<EventAssociatedWithEntity> EventToAssociatedWithEntity(Node node)
+        {
+            var result = new JObject(new JProperty(nameof(node.Id).ToLower(), node.Id.ToString("N")));
+
+            var importanceId = node.Nodes.FirstOrDefault(p => p.Type.Name == "importance").Nodes.First().Id;
+            var stateId = node.Nodes.FirstOrDefault(p => p.Type.Name == "state").Nodes.First().Id;
+
+            var importanceAttributes = (await _ontologyService.LoadNodesAsync(importanceId, null)).GetChildAttributes();
+            var stateAttributes = (await _ontologyService.LoadNodesAsync(stateId, null)).GetChildAttributes();
+
+            var attributies = node.GetChildAttributes();
+
+            var res = new EventAssociatedWithEntity {
+                Id = node.Id,
+                Name = attributies.FirstOrDefault(p => p.dotName == "name").attribute?.Value as string,
+                Description = attributies.FirstOrDefault(p => p.dotName == "description").attribute?.Value as string,
+                UpdatedAt = attributies.FirstOrDefault(p => p.dotName == "updatedAt").attribute?.Value as DateTime?,
+                StartsAt = attributies.FirstOrDefault(p => p.dotName == "startsAt").attribute?.Value as DateTime?,
+                EndsAt = attributies.FirstOrDefault(p => p.dotName == "endsAt").attribute?.Value as DateTime?,
+                State = new EventState {
+                    Id = stateId,
+                    Name = stateAttributes.FirstOrDefault(p => p.dotName == "name").attribute?.Value as string,
+                    Code = stateAttributes.FirstOrDefault(p => p.dotName == "code").attribute?.Value as string
+                },
+                Importance = new EventImportance
+                {
+                    Id = importanceId,
+                    Name = importanceAttributes.FirstOrDefault(p => p.dotName == "name").attribute?.Value as string,
+                    Code = importanceAttributes.FirstOrDefault(p => p.dotName == "code").attribute?.Value as string
+                }
+            };
+
+            return res;
+        }
+
         public JObject EventToJObject(Node node)
         {
             var result = new JObject(new JProperty(nameof(node.Id).ToLower(), node.Id.ToString("N")));
 
-            var attributies = node.GetChildAttributes().Where(a => a.dotName == "name" || a.dotName == "description");
+            var attributies = node.GetChildAttributes()
+                .Where(a => EventDotNames.Contains(a.dotName));
 
             foreach (var attribute in attributies)
             {
