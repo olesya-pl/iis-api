15bd705ac431605d433d4861541bcf869af525c5
Taras Mykhats <tmykhats@warfare-tec.com>
2020-09-17 20:17:00 +0300

    build fixes

--- src/Iis.Services.Contracts/Interfaces/IElasticState.cs
+++ src/Iis.Services.Contracts/Interfaces/IElasticState.cs
@@ -4,6 +4,7 @@ namespace Iis.Services.Contracts.Interfaces
 {
     public interface IElasticState
     {
+        bool UseElastic { get; }
         List<string> EventIndexes { get; }
         List<string> FeatureIndexes { get; }
         Dictionary<string, string> HistoricalOntologyIndexes { get; }
