33d7ead598f94ab0e86e0520c683cc2a51499cc3
Taras Mykhats <tmykhats@warfare-tec.com>
2020-11-13 12:43:38 +0200

    fix issue with update database

--- src/Iis.Api/BackgroundServices/ThemeCounterBackgroundService.cs
+++ src/Iis.Api/BackgroundServices/ThemeCounterBackgroundService.cs
@@ -5,28 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Iis.ThemeManagement;
 using Iis.DbLayer.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Iis.Api.BackgroundServices
 {
     public class ThemeCounterBackgroundService : BackgroundService
     {
         private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<ThemeCounterBackgroundService> _logger;
 
-        public ThemeCounterBackgroundService(IServiceProvider serviceProvider)
+        public ThemeCounterBackgroundService(IServiceProvider serviceProvider, ILogger<ThemeCounterBackgroundService> logger)
         {
             _serviceProvider = serviceProvider;
+            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                using (var scope = _serviceProvider.CreateScope())
+                try
                 {
-                    var themeService = scope.ServiceProvider.GetRequiredService<ThemeService<IIISUnitOfWork>>();
-                    await themeService.UpdateQueryResultsAsync(stoppingToken);
-                } 
-                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+                    using (var scope = _serviceProvider.CreateScope())
+                    {
+                        var themeService = scope.ServiceProvider.GetRequiredService<ThemeService<IIISUnitOfWork>>();
+                        await themeService.UpdateQueryResultsAsync(stoppingToken);
+                    }
+                    await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+                }
+                catch (Exception e) 
+                {
+                    _logger.LogError(e, $"Error occur while {nameof(ThemeCounterBackgroundService)} excution");
+                    await Task.Delay(TimeSpan.FromMinutes(2));
+                }
             }
         }
     }
