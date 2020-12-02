ffa2b1da019c274d09678f65f71735c7b4f85b96
Bohdan Stupak <stupakbogdan@ukr.net>
2020-12-02 16:31:08 +0200

    feat: username in change history
    relates to: IIS-6664

---
+++ src/Iis.DbLayer/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Iis.DataModel;
+using IIS.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iis.DbLayer.Repositories
+{
+    internal class UserRepository : RepositoryBase<OntologyContext>, IUserRepository
+    {
+        public Task<UserEntity> GetByIdAsync(Guid userId)
+        {
+            return Context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == userId);
+        }
+    }
+}
