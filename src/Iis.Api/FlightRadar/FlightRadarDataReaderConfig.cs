720558058d65c095de2d2026f4c367c35236e410
Bohdan Stupak <bstupak@infozahyst.com>
2021-01-26 11:04:58 +0200

    Resolve IIS-7094 "Flight radar export"

---
+++ src/Iis.Api/FlightRadar/FlightRadarDataReaderConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Iis.Api.FlightRadar
+{
+    public class FlightRadarDataReaderConfig
+    {
+        public string DataFolder { get; set; }
+        public string ProcessedFolder { get; set; }
+        public string ErrorFolder { get; set; }
+    }
+}
---
+++ src/Iis.Api/FlightRadar/FlightRadarRegistrationModule.cs
@@ -0,0 +1,22 @@
+﻿using Iis.DbLayer.Repositories;
+using IIS.Core.FlightRadar;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iis.Api.FlightRadar
+{
+    public static class FlightRadarRegistrationModule
+    {
+        public static IServiceCollection RegisterFlightRadarServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var flightRadarDataReaderConfig = configuration.GetSection("flightRadarDataReader").Get<FlightRadarDataReaderConfig>();
+            services.AddSingleton(flightRadarDataReaderConfig);
+
+            services.AddTransient<IFlightRadarService, FlightRadarService<IIISUnitOfWork>>();
+            services.AddHostedService<FlightRadarHistorySyncJob>();
+            services.AddHostedService<FlightRadarDataImporter>();
+
+            return services;
+        }
+    }
+}
