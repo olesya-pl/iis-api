4a8fc4f673f3968ed775084ea73862e8a240842e
Taras Mykhats <tmykhats@infozahyst.com>
2021-02-04 17:08:35 +0200

    made TowerLocations keyless(added missed migration files)

---
+++ src/Iis.DataModel/Migrations/20210204150751_MakeTowerLocationKeyless.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IIS.Core.Migrations
+{
+    public partial class MakeTowerLocationKeyless : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TowerLocations",
+                table: "TowerLocations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TowerLocations_Mcc_Mnc_Lac_CellId",
+                table: "TowerLocations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TowerLocations",
+                table: "TowerLocations",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TowerLocations_Mcc_Mnc_Lac_CellId",
+                table: "TowerLocations",
+                columns: new[] { "Mcc", "Mnc", "Lac", "CellId" })
+                .Annotation("Npgsql:IndexInclude", new[] { "Lat", "Long" });
+        }
+    }
+}
