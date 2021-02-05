9c1b60b914b34a8bdea240027019e9fcd6650a78
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-02-05 15:57:05 +0200

    feat(ontologyManager): update of compare screen

---
+++ src/Iis.OntologyManager/Comparison/CompareResultForGridItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.OntologyManager.Comparison
+{
+    public enum CompareResultOperation
+    {
+        Insert,
+        Update,
+        Delete
+    };
+
+    public enum CompareResultItemType
+    {
+        NodeType,
+        Alias
+    };
+
+    public class CompareResultForGridItem
+    {
+        public bool Checked { get; set; }
+        public string Title { get; set; }
+        public string NewValue { get; set; }
+        public string OldValue { get; set; }
+        public CompareResultOperation Operation { get; set; }
+        public CompareResultItemType ItemType { get; set; }
+        public object LinkedObject { get; set; }
+    }
+}
