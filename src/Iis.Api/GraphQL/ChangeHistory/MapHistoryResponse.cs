d625d1823f2fefdde0a14b2625da6dafb65f92dd
Bohdan Stupak <bstupak@infozahyst.com>
2021-02-08 16:08:11 +0200

    Iis map change history

---
+++ src/Iis.Api/GraphQL/ChangeHistory/MapHistoryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace IIS.Core.GraphQL.ChangeHistory
+{
+    public class MapHistoryResponse
+    {
+        public List<ChangeHistoryItemGroup> GeoItems { get; set; }
+        public List<HistoryItemMonthlyAggregate> MonthlyAggregates { get; set; }
+    }
+}
