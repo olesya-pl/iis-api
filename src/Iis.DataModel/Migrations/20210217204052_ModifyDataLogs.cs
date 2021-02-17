2d51a1a3d43d643b7904780eac48fe44dcbae40f
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-02-17 23:19:14 +0200

    feat(ontology): ModifyDataScripts and remove event-wiki links
    Relates to IIS-7294

---
+++ src/Iis.DataModel/Migrations/20210217204052_ModifyDataLogs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IIS.Core.Migrations
+{
+    public partial class ModifyDataLogs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModifyDataLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    RunDate = table.Column<DateTime>(nullable: false),
+                    Success = table.Column<bool>(nullable: false),
+                    Error = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModifyDataLogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModifyDataLogs");
+        }
+    }
+}
