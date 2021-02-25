3ff93f135ee0f883d92083f0aa2556d39f6558a1
admin <admin@TeamCity>
2021-02-25 19:01:04 +0200

    TeamCity change in 'IIS / Core / API' project: parameters of 'Build Docker' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -52,7 +52,6 @@ object Api_BuildDocker : BuildType({
     name = "Build Docker"
 
     params {
-        param("DOCKER_IMAGE_NAME", "docker.contour.net:5000/iis-core")
         param("env.CI_BUILD_VERSION", "%teamcity.build.branch%.%system.build.number%")
         param("gitHashShort", "ffffffff")
     }
