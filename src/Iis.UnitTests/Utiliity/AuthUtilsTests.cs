8ac8e852629dfe7da2f9fa3299904c5d702c742d
Bohdan Stupak <stupakbogdan@ukr.net>
2021-03-02 13:20:43 +0200

    feat: basic auth utility
    relates to: IIS-7307

---
+++ src/Iis.UnitTests/Utiliity/AuthUtilsTests.cs
@@ -0,0 +1,15 @@
+﻿using Iis.Utility;
+using Xunit;
+
+namespace Iis.UnitTests.Utiliity
+{
+    public class AuthUtilsTests
+    {
+        [Theory]
+        [InlineData("elastic", "112233", "Basic ZWxhc3RpYzoxMTIyMzM=")]
+        public void GenerateBasicAuthHeaderValue(string login, string password, string expected)
+        {
+            Assert.Equal(expected, AuthUtils.GenerateBasicAuthHeaderValue(login, password));
+        }
+    }
+}
