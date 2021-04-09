172f9e9a0c83c43d821698d2a1ccdf3cdc8e0e24
admin <admin@TeamCity>
2021-04-09 13:54:34 +0300

    TeamCity change in 'IIS / Core / Tests' project: runners of 'IIS Acceptance Tests(Sanity)' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -530,6 +530,7 @@ object Tests_IisAcceptanceTestsSanity : BuildType({
             filter = "%test_filter%"
             logging = DotnetTestStep.Verbosity.Detailed
             dockerImagePlatform = DotnetTestStep.ImagePlatform.Linux
+            dockerPull = true
             dockerImage = "mcr.microsoft.com/dotnet/core/sdk:3.1"
             dockerRunParameters = "-e TargetEnvironment=Dev3"
             param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
