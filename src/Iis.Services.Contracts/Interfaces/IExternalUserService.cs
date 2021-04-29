7cdea7a9ca9c30e383d32e2110427d125f7c2473
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-04-29 14:05:28 +0300

    feat(access): external users
    Relates to IIS-7524

---
+++ src/Iis.Services.Contracts/Interfaces/IExternalUserService.cs
@@ -0,0 +1,12 @@
+﻿using Iis.Services.Contracts.ExternalUserServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.Services.Contracts.Interfaces
+{
+    public interface IExternalUserService
+    {
+        List<ExternalUser> GetUsers();
+    }
+}
---
+++ src/Iis.Services/ExternalUserServices/ActiveDirectoryUserService.cs
@@ -0,0 +1,12 @@
+﻿using Iis.Services.Contracts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.Services.ExternalUserServices
+{
+    public class ActiveDirectoryUserService: IExternalUserService
+    {
+
+    }
+}
