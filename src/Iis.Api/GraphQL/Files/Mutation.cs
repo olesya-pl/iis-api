4f230be14a8e9c37a166daeba5e24616462051d1
Bohdan Stupak <stupakbogdan@ukr.net>
2021-05-24 18:27:50 +0300

    feat: temp commit
    relates to: IIS-7680

--- src/Iis.Api/GraphQL/Files/Mutation.cs
+++ src/Iis.Api/GraphQL/Files/Mutation.cs
@@ -86,46 +86,7 @@ namespace IIS.Core.GraphQL.Files
             }
         }
 
-        private static async Task<UploadResult> UploadPng(IFileService fileService, IMaterialService materialService, UploadInput input, User user)
-        {
-            using (var stream = new MemoryStream(input.Content))
-            {
-                FileIdDto fileSaveResult = await fileService
-                .SaveFileAsync(stream, input.Name, "image/png", CancellationToken.None);
-                if (fileSaveResult.IsDuplicate)
-                {
-                    return DuplicatedUploadResult;
-                }
-
-                var loadData = new MaterialLoadData();
-
-                if(user != null)
-                {
-                    loadData.LoadedBy = $"{user.LastName} {user.FirstName} {user.Patronymic}";
-                }
-
-                var material = new Material
-                {
-                    Id = Guid.NewGuid(),
-                    Title = input.Name,
-                    Type = "image",
-                    Source = "iis.api",
-                    Metadata = JObject.FromObject(new
-                    {
-                        source = "iis.api",
-                        type = "image"
-                    }),
-                    LoadData = loadData,
-                    File = new FileDto(fileSaveResult.Id),
-                    CreatedDate = DateTime.UtcNow,
-                    AccessLevel = input.AccessLevel,
-                };
-
-                await materialService.SaveAsync(material);
-
-                return new UploadResult { Success = true };
-            }
-        }
+        
 
         private static async Task<UploadResult> UploadFileAsync(IFileService fileService, string directory, UploadInput input, User user)
         {
---
+++ src/Iis.Services/UploadService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Iis.DbLayer.Repositories;
+using Iis.Domain.Materials;
+using Iis.Services.Contracts;
+using Iis.Services.Contracts.Dtos;
+using Iis.Services.Contracts.Interfaces;
+using Newtonsoft.Json.Linq;
+using MaterialLoadData = Iis.Domain.Materials.MaterialLoadData;
+
+namespace Iis.Services
+{
+    public enum UploadResult
+    {
+        Success,
+        Duplicate
+    }
+    
+    public class UploadService
+    {
+        private readonly IFileService _fileService;
+
+        public UploadService(IFileService fileService, IMaterialService materialService)
+        {
+            _fileService = fileService;
+        }
+        
+        public async Task<UploadResult> UploadPng(UploadInput input, User user)
+        {
+            using (var stream = new MemoryStream(input.Content))
+            {
+                var fileSaveResult = await _fileService
+                .SaveFileAsync(stream, input.Name, "image/png", CancellationToken.None);
+                if (fileSaveResult.IsDuplicate)
+                {
+                    return UploadResult.Duplicate;
+                }
+
+                var loadData = new MaterialLoadData();
+
+                if (user != null)
+                {
+                    loadData.LoadedBy = $"{user.LastName} {user.FirstName} {user.Patronymic}";
+                }
+
+                var material = new Material
+                {
+                    Id = Guid.NewGuid(),
+                    Title = input.Name,
+                    Type = "image",
+                    Source = "iis.api",
+                    Metadata = JObject.FromObject(new
+                    {
+                        source = "iis.api",
+                        type = "image"
+                    }),
+                    LoadData = loadData,
+                    File = new FileDto(fileSaveResult.Id),
+                    CreatedDate = DateTime.UtcNow,
+                    AccessLevel = input.AccessLevel,
+                };
+
+                await materialService.SaveAsync(material);
+
+                return UploadResult.Success;
+            }
+        }
+    }
+}
