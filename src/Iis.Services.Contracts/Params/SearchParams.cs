52e2d58706fcecce8c0d8d9a67ce9f631aa1902a
INFOZAHYST\bstupak <bstupak@warfare-tec.com>
2021-06-07 17:53:02 +0300

    feat: transform new properties to query string
    relates to: IIS-7779

--- src/Iis.Services.Contracts/Params/SearchParams.cs
+++ src/Iis.Services.Contracts/Params/SearchParams.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+
+using Iis.Interfaces.Elastic;
+
 namespace Iis.Services.Contracts.Params
 {
     public class SearchParams
@@ -5,5 +9,7 @@ namespace Iis.Services.Contracts.Params
         public string Suggestion { get; set; }
         public PaginationParams Page {get;set;}
         public SortingParams Sorting { get; set; }
+        public List<CherryPickedItem> CherryPickedItems { get; set; } = new List<CherryPickedItem>();
+        public List<Property> FilteredItems { get; set; } = new List<Property>();
     }
 }
