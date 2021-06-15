0c2108f779fbed155a4c83e140a2ca6d80655f86
Bohdan Stupak <stupakbogdan@ukr.net>
2021-06-15 12:54:25 +0300

    add dockerfile

--- src/Iis.Api/Configuration/MqConfiguration.cs
+++ src/Iis.Api/Configuration/MqConfiguration.cs
@@ -9,5 +9,6 @@ namespace Iis.Api.Configuration
         public string VirtualHost { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public int Port { get; set; }
     }
 }
