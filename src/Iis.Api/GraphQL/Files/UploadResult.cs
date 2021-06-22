0e9f89d702cebf7e9630f87320dc97979f6a894e
Mykhailo Demydenko <mdemydenko@infozahyst.com>
2021-06-22 15:01:51 +0300

    [IIS-7748] [BE] Завантаження матеріалів > Ошибка при загрузке .PNG

--- src/Iis.Api/GraphQL/Files/UploadResult.cs
+++ src/Iis.Api/GraphQL/Files/UploadResult.cs
@@ -4,5 +4,13 @@
     {
         public bool Success { get; set; }
         public string Message { get; set; }
+
+        private static UploadResult _successResult = new UploadResult { Success = true };
+        private static UploadResult _unsupportedResult = Error("Формат не підтримується");
+        private static UploadResult _duplicatedResult = Error("Даний файл вже завантажений до системи");
+        public static UploadResult Ok => _successResult;
+        public static UploadResult Unsupported => _unsupportedResult;
+        public static UploadResult Duplicated => _duplicatedResult;
+        public static UploadResult Error(string message) => new UploadResult { Success = false, Message = message };
     }
 }
