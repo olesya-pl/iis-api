9192ecbbb1b61079775d81e3f3fb815c2c195b7a
Bohdan Stupak <bstupak@infozahyst.com>
2021-07-20 14:27:14 +0300

    fix: add osint processor
    relates to: IIS-7834

--- src/Iis.Api/Materials/FeatureProcessors/IFeatureProcessor.cs
+++ src/Iis.Api/Materials/FeatureProcessors/IFeatureProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
@@ -11,5 +12,6 @@ namespace IIS.Core.Materials.FeatureProcessors
     {
         bool IsDummy { get; }
         Task<JObject> ProcessMetadataAsync(JObject metadata, Guid materialId);
+        IEnumerable<Guid> GetValidFeatureIds(IEnumerable<Guid> featureIdList);
     }
 }
