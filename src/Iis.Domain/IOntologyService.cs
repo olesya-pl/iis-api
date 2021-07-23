70ced854e38b4f6d97003e630dddc60a9bbaf83f
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-07-23 04:34:58 +0300

    Relates to IIS-7751

--- src/Iis.Domain/IOntologyService.cs
+++ src/Iis.Domain/IOntologyService.cs
@@ -6,6 +6,7 @@ using Iis.Interfaces.Ontology.Data;
 using Iis.Interfaces.Elastic;
 using Iis.Interfaces.Ontology.Schema;
 using Iis.Domain.Users;
+using Iis.Domain.TreeResult;
 
 namespace Iis.Domain
 {
@@ -31,5 +32,6 @@ namespace Iis.Domain
         string GetAttributeValueByDotName(Guid id, string dotName);
         Dictionary<Guid, int> GetRelationsCount(HashSet<Guid> entityIds);
         Task<SearchEntitiesByConfiguredFieldsResult> SearchEventsAsync(ElasticFilter filter, User user);
+        TreeResultList GetEventTypes(string suggestion);
     }
 }
---
+++ src/Iis.Domain/TreeResult/TreeResult.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.Domain.TreeResult
+{
+    public class TreeResult
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+        public List<TreeResult> Options { get; set; } = new List<TreeResult>();
+        public override string ToString() => Label;
+        public JObject GetJsonObject()
+        {
+            var result = new JObject();
+            
+            result["label"] = Label;
+
+            if (!string.IsNullOrEmpty(Value))
+                result["value"] = Value;
+
+            if (Options.Count > 0)
+            {
+                var jOptions = new JArray();
+                foreach (var option in Options)
+                {
+                    jOptions.Add(option.GetJsonObject());
+                }
+                result["options"] = jOptions;
+            }
+
+            return result;
+        }
+    }
+}
