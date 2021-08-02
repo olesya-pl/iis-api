eda6e9f35da39df3d51c7fc0305e793859480e42
mdemydenko <mdemydenko@warfare-tec.com>
2021-08-02 12:51:23 +0300

    fix material.created messaging for image upload

--- src/Iis.Messages/MaterialRabbitConsts.cs
+++ src/Iis.Messages/MaterialRabbitConsts.cs
@@ -2,6 +2,7 @@ namespace Iis.Messages
 {
     public class MaterialRabbitConsts
     {
+        public const string DefaultExchangeName = "materials";
         public const string QueueName = "material.created";
     }
 }
