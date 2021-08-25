a92ab60105aeedde9a994406db8aebc3c61ac554
Bohdan Stupak <stupakbogdan@ukr.net>
2021-08-25 16:20:29 +0300

    dockerfile update

--- src/Iis.Api/Modules/MqRegistrationModule.cs
+++ src/Iis.Api/Modules/MqRegistrationModule.cs
@@ -25,8 +25,10 @@ namespace Iis.Api.Modules
                 Password = mqConfig.Password ?? ConnectionFactory.DefaultPass,
                 VirtualHost = mqConfig.VirtualHost ?? ConnectionFactory.DefaultVHost,
             };
-            
-            mqConnectionString = $"amqp://{connectionFactory.UserName}:{connectionFactory.Password}@{connectionFactory.HostName}";
+
+            var portString = connectionFactory.Port == -1 ? string.Empty :
+                ":" + connectionFactory.Port;
+            mqConnectionString = $"amqp://{connectionFactory.UserName}:{connectionFactory.Password}@{connectionFactory.HostName}{portString}/{connectionFactory.VirtualHost}";
 
             return services
                     .AddTransient<IConnectionFactory>(serviceProvider => connectionFactory);
