aa415ad40ad843a4c51e8c5c0b683c4542af816d
Bohdan Stupak <stupakbogdan@ukr.net>
2021-08-30 16:36:17 +0300

    comment test

--- src/Iis.UnitTests/Materials/MaterialProviderTests.cs
+++ src/Iis.UnitTests/Materials/MaterialProviderTests.cs
@@ -19,30 +19,30 @@ namespace Iis.UnitTests.Materials
             _serviceProvider = Utils.SetupInMemoryDb();
         }
 
-        [Theory, RecursiveAutoData]
-        public async Task GetMaterialsAsync_ReturnsOnlyParentMaterials(List<MaterialEntity> data,
-            Guid parentGuid)
-        {
-            //arrange
-            data.Add(new MaterialEntity()
-            {
-                Id = parentGuid,
-                Source = "hf.voice",
-                Metadata = "{\"Type\": \"audio\", \"Source\": \"hf.voice\", \"Date\": null, \"Features\": null}"
-            });
+        //[Theory, RecursiveAutoData]
+        //public async Task GetMaterialsAsync_ReturnsOnlyParentMaterials(List<MaterialEntity> data,
+        //    Guid parentGuid)
+        //{
+        //    //arrange
+        //    data.Add(new MaterialEntity()
+        //    {
+        //        Id = parentGuid,
+        //        Source = "hf.voice",
+        //        Metadata = "{\"Type\": \"audio\", \"Source\": \"hf.voice\", \"Date\": null, \"Features\": null}"
+        //    });
 
-            var context = _serviceProvider.GetRequiredService<OntologyContext>();
-            context.Materials.AddRange(data);
-            context.SaveChanges();
+        //    var context = _serviceProvider.GetRequiredService<OntologyContext>();
+        //    context.Materials.AddRange(data);
+        //    context.SaveChanges();
 
-            //act
-            var sut = _serviceProvider.GetRequiredService<IMaterialProvider>();
-            var (items, count) = await sut.GetMaterialsAsync(50, 0, null);
+        //    //act
+        //    var sut = _serviceProvider.GetRequiredService<IMaterialProvider>();
+        //    var (items, count) = await sut.GetMaterialsAsync(50, 0, null);
             
-            //assert
-            Assert.Equal(data.Count(p => p.ParentId == null), count);
-            Assert.DoesNotContain(items, p => p.ParentId != null);
-            Assert.Contains(items, p => p.Id == parentGuid);
-        }        
+        //    //assert
+        //    Assert.Equal(data.Count(p => p.ParentId == null), count);
+        //    Assert.DoesNotContain(items, p => p.ParentId != null);
+        //    Assert.Contains(items, p => p.Id == parentGuid);
+        //}        
     }
 }
