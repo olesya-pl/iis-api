87408dd695a5174759ddfddf505571dcebe8f776
vtkhozhevskyi <vtkhozhevskyi@infozahyst.com>
2021-08-31 16:44:24 +0300

    fix: resolve registration date for all types
    Closes IIS-8037

--- src/Iis.MaterialLoader/Automapper/Resolvers/MaterialRegistrationDateResolver.cs
+++ src/Iis.MaterialLoader/Automapper/Resolvers/MaterialRegistrationDateResolver.cs
@@ -9,24 +9,20 @@ namespace Iis.MaterialLoader.Automapper.Resolvers
 {
     public class MaterialRegistrationDateResolver : IMemberValueResolver<MaterialInput, Domain.Materials.Material, string, DateTime?>
     {
-        private const string Type = "type";
         private const string Source = "source";
 
         public DateTime? Resolve(MaterialInput source, Domain.Materials.Material destination, string sourceMember, DateTime? destMember, ResolutionContext context)
         {
             if (string.IsNullOrWhiteSpace(sourceMember)
                 || !sourceMember.TryParseToJObject(out var parsed)
-                || !parsed.TryGetValue(Type, StringComparison.InvariantCultureIgnoreCase, out var typeJObject)
                 || !parsed.TryGetValue(Source, StringComparison.InvariantCultureIgnoreCase, out var sourceJObject))
                 return default;
 
-            string materialType = typeJObject.Value<string>();
             string materialSource = sourceJObject.Value<string>();
-            if (string.IsNullOrWhiteSpace(materialType)
-                || string.IsNullOrWhiteSpace(materialSource))
+            if (string.IsNullOrWhiteSpace(materialSource))
                 return default;
 
-            var resolver = RegistrationDateResolverFactory.Create(materialType, materialSource);
+            var resolver = RegistrationDateResolverFactory.Create(materialSource);
 
             resolver.TryResolve(parsed, out DateTime? registrationDate);
 
--- src/Iis.MaterialLoader/Helpers/RegistrationDateResolvers/RegistrationDateResolverFactory.cs
+++ src/Iis.MaterialLoader/Helpers/RegistrationDateResolvers/RegistrationDateResolverFactory.cs
@@ -4,15 +4,15 @@ namespace Iis.MaterialLoader.Helpers.RegistrationDateResolvers
 {
     public static class RegistrationDateResolverFactory
     {
-        public static IMaterialRegistrationDateResolver Create(string type, string source)
+        public static IMaterialRegistrationDateResolver Create(string source)
         {
-            return (type, source) switch
+            return (source) switch
             {
-                (Material.Types.Audio, Material.Sources.CellVoice) => new CellVoiceRegistrationDateResolver(),
-                (Material.Types.Audio, Material.Sources.SatVoice) => new SatVoiceRegistrationDateResolver(),
-                (Material.Types.Audio, Material.Sources.SatIridiumVoice) => new SatIridiumVoiceRegistrationDateResolver(),
-                (Material.Types.Audio, Material.Sources.HfVoice) => new HfVoiceRegistrationDateResolver(),
-                (_, _) => new DummyRegistrationDateResolver()
+                Material.Sources.CellVoice => new CellVoiceRegistrationDateResolver(),
+                Material.Sources.SatVoice => new SatVoiceRegistrationDateResolver(),
+                Material.Sources.SatIridiumVoice => new SatIridiumVoiceRegistrationDateResolver(),
+                Material.Sources.HfVoice => new HfVoiceRegistrationDateResolver(),
+                _ => new DummyRegistrationDateResolver()
             };
         }
     }
