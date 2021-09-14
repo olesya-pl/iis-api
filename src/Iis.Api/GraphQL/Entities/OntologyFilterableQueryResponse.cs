6e8f64a7e44cae69dbf72612928b580abb89b900
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-09-14 15:57:21 +0300

    Relates to IIS-7651

--- src/Iis.Api/GraphQL/Entities/OntologyFilterableQueryResponse.cs
+++ src/Iis.Api/GraphQL/Entities/OntologyFilterableQueryResponse.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using HotChocolate;
 using HotChocolate.Types;
 using IIS.Core.GraphQL.Scalars;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace IIS.Core.GraphQL.Entities
@@ -13,5 +15,13 @@ namespace IIS.Core.GraphQL.Entities
         public int Count { get; set; }
         public Dictionary<string, AggregationItem> Aggregations { get; set; }
         public IReadOnlyList<AggregationNodeTypeItem> NodeTypeAggregations { get; set; }
+        public JObject NodeTypeAggregationsJObject
+        {
+            get
+            {
+                var result = JObject.FromObject(NodeTypeAggregations);
+                return result;
+            }
+        }
     }
 }
