3df1852e3647bc173b0e91dea39458cb41df9aa4
Bohdan Stupak <stupakbogdan@ukr.net>
2021-09-27 17:51:45 +0300

    feat: introduce shortcut to omit costly regex

---
+++ src/Iis.UnitTests/Iis.Api/GraphQL/Access/GraphQLAccessItemTests.cs
@@ -0,0 +1,31 @@
+﻿using Iis.Api.GraphQL.Access;
+using Iis.Interfaces.Roles;
+using Xunit;
+
+namespace Iis.UnitTests.Iis.Api.GraphQL.Access
+{
+    public class GraphQLAccessItemTests
+    {
+        [Fact]
+        public void IsMatch_ItemsEqual_ReturnsTrue()
+        {
+            var sut = new GraphQLAccessItem(AccessKind.FreeForAll, AccessOperation.None, @"getEntityTypes", @"getEntityTypeIcons");
+            Assert.True(sut.IsMatch("getEntityTypes"));
+        }
+
+        [Fact]
+        public void IsMatch_NoMatchingItems_ReturnsFalse()
+        {
+            var sut = new GraphQLAccessItem(AccessKind.FreeForAll, AccessOperation.None, @"getEntityTypes", @"getEntityTypeIcons");
+            Assert.False(sut.IsMatch("getEntityIcons"));
+        }
+
+        [Fact]
+        public void IsMatch_RegexIsRespected()
+        {
+            var sut = new GraphQLAccessItem(AccessKind.Entity, AccessOperation.Create, @"createEntity(?!Event)");
+            Assert.False(sut.IsMatch("createEntityEvent"));
+            Assert.True(sut.IsMatch("createEntityMilitaryOrganization"));
+        }
+    }
+}
