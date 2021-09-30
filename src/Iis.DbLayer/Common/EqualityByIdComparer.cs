0f6697869c9a095bd1cd68a1b5fca385865e8675
Yuriy Dolzhykov <ydolzhykov@warfare-tec.com>
2021-09-30 16:32:38 +0300

    Duplicates in related Materials
    Relates to IIS-8108

---
+++ src/Iis.DbLayer/Common/EqualityByIdComparer.cs
@@ -0,0 +1,15 @@
+﻿using Iis.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iis.DbLayer.Common
+{
+    public class EqualityByIdComparer: IEqualityComparer<BaseEntity>
+    {
+        public bool Equals(BaseEntity entity1, BaseEntity entity2) =>
+            entity1?.Id == entity2?.Id;
+
+        public int GetHashCode(BaseEntity entity) => entity.Id.GetHashCode();
+    }
+}
