1e50bfa743253b85779a13712c8453ab41bd9d01
Bohdan Stupak <stupakbogdan@ukr.net>
2021-12-02 17:03:38 +0200

    fix: respect time in range
    relates to: IIS-8379

---
+++ src/Iis.UnitTests/ElasticService/CreateMaterialQueryStringTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Iis.Interfaces.Elastic;
+using Iis.Interfaces.Materials;
+using Iis.Interfaces.Common;
+using Xunit;
+using Iis.Elastic.SearchQueryExtensions;
+using FluentAssertions;
+
+namespace Iis.UnitTests.CreateMaterialQueryStringTests
+{
+    public class CreateMaterialQueryStringTests
+    {
+        [Fact]
+        public void DateRangeIsProvided_HoursAndMinutesRespected()
+        {
+            //arrange
+            var searchParams = new SearchParams
+            {
+                Page = new PaginationParams(1, 50),
+                CreatedDateRange = new DateRange(new DateTime(2021, 11, 18, 10, 00, 00), new DateTime(2021, 11, 18, 12, 00, 00))
+            };
+
+            //act
+            var queryString = SearchQueryExtension.CreateMaterialsQueryString(searchParams);
+
+            //assert
+            queryString.Should().Be("((ParentId:NULL)) AND CreatedDate:[2021-11-18T10:00:00Z TO 2021-11-18T12:00:00Z]");
+        }
+    }
+}
