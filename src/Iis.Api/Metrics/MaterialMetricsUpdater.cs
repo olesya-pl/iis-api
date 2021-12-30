f5a45c9d52fae9ab5e537d248b4fe772648564da
vtkhozhevskyi <vtkhozhevskyi@infozahyst.com>
2021-12-30 15:45:24 +0200

    feat: added material and health metrics
    Closes IIS-8532

---
+++ src/Iis.Api/Metrics/MaterialMetricsUpdater.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Iis.DataModel;
+using Iis.Interfaces.Metrics;
+using Iis.Metrics.Materials;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Iis.Api.Metrics
+{
+    public class MaterialMetricsUpdater
+    {
+        private readonly ILogger<MaterialMetricsManager> _logger;
+        private readonly OntologyContext _ontologyContext;
+        private readonly IMaterialMetricsManager _materialMetricsManager;
+
+        public MaterialMetricsUpdater(
+            ILogger<MaterialMetricsManager> logger,
+            OntologyContext ontologyContext,
+            IMaterialMetricsManager materialMetricsManager)
+        {
+            _logger = logger;
+            _ontologyContext = ontologyContext;
+            _materialMetricsManager = materialMetricsManager;
+        }
+
+        public async Task UpdateAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = await _ontologyContext.Materials
+                    .AsNoTracking()
+                    .Where(_ => _.ParentId == null)
+                    .GroupBy(_ => _.Source)
+                    .Select(_ => new
+                    {
+                        Type = _.Key,
+                        Count = _.LongCount()
+                    })
+                    .ToArrayAsync(cancellationToken);
+                var materialMetrics = new MaterialMetrics
+                {
+                    TotalCount = data.Sum(_ => _.Count),
+                    BySourceCounts = data.ToDictionary(_ => _.Type, _ => _.Count)
+                };
+
+                _materialMetricsManager.SetMetrics(materialMetrics);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Cannot update material metrics: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Iis.Interfaces/Metrics/MaterialMetrics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Iis.Interfaces.Metrics
+{
+    public class MaterialMetrics
+    {
+        public long TotalCount { get; set; }
+        public IReadOnlyDictionary<string, long> BySourceCounts { get; set; }
+    }
+}
