0d38c87b01edf3e4b183de3020c8c74a10b17f69
vtkhozhevskyi <vtkhozhevskyi@infozahyst.com>
2022-01-21 17:38:21 +0200

    fix: init on startup
    Closes IIS-8532

---
+++ src/Iis.Api/Metrics/HostExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Iis.Interfaces.Metrics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Iis.Api.Metrics
+{
+    internal static class HostExtensions
+    {
+        public static IHost InitAppMetrics(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var materialMetricsManager = scope.ServiceProvider.GetRequiredService<IMaterialMetricsManager>();
+
+            materialMetricsManager.SetDefaultValues();
+
+            return host;
+        }
+    }
+}
