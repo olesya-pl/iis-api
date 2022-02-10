f3a8b9bbe65919c9fe557de3a92cb6073035980b
Bohdan Stupak <bstupak@infozahyst.com>
2022-02-10 15:58:46 +0000

    apply custom similarity

--- src/Iis.Elastic/ElasticMappingProperties/TextProperty.cs
+++ src/Iis.Elastic/ElasticMappingProperties/TextProperty.cs
@@ -11,22 +11,23 @@ namespace Iis.Elastic.ElasticMappingProperties
         private const int IgnoreAbove = 256;
         private TextTermVectorsEnum _termVector;
         private bool _useNestedKeyword;
+        private bool _useCustomSimilarity;
 
         private TextProperty() { }
 
         public override ElasticMappingPropertyType Type => ElasticMappingPropertyType.Text;
 
-        public static ElasticMappingProperty Create(string propertyName, TextTermVectorsEnum termVector, bool useNestedKeyword = false)
+        public static ElasticMappingProperty Create(string propertyName, TextTermVectorsEnum termVector, bool useNestedKeyword = false, bool useCustomSimilarity = false)
         {
             return CreateWithNestedProperty(
                 propertyName,
-                (propName) => new TextProperty { Name = propName, _termVector = termVector, _useNestedKeyword = useNestedKeyword },
+                (propName) => new TextProperty { Name = propName, _termVector = termVector, _useNestedKeyword = useNestedKeyword, _useCustomSimilarity = useCustomSimilarity },
                 (propName) => Create(propName, termVector, useNestedKeyword));
         }
 
-        public static ElasticMappingProperty Create(string propertyName, bool useNestedKeyword = false)
+        public static ElasticMappingProperty Create(string propertyName, bool useNestedKeyword = false, bool useCustomSimilarity = false)
         {
-            return Create(propertyName, TextTermVectorsEnum.No, useNestedKeyword);
+            return Create(propertyName, TextTermVectorsEnum.No, useNestedKeyword, useCustomSimilarity);
         }
 
         protected override void PopulatePropertyIntoJObject(JObject result)
@@ -43,6 +44,10 @@ namespace Iis.Elastic.ElasticMappingProperties
                         new JProperty("type", "keyword"),
                         new JProperty(IgnoreAbovePropName, IgnoreAbove))));
                 result.Add("fields", field);
+            }
+            if (_useCustomSimilarity)
+            {
+                result["similarity"] = ElasticConstants.CustomSimilarityFunctionName;
             }
         }
     }
